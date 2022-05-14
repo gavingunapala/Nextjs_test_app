beb686dba1626d9907ad33787face22ba9297fb1
gavin <gavingunapala06@gmail.com>
2022-05-14 12:21:11 +0530

    add data created

--- models/deliveryModel.js
+++ models/deliveryModel.js
@@ -1,18 +1,39 @@
 const mongoose = require("mongoose");
 
 const NoteSchema = new mongoose.Schema({
-  title: {
+  userId: {
     type: String,
     required: true,
   },
-//   todaydate: {
-//     type: String,
-//     required: true,
-//   },
-//   body: {
-//     type: String,
-//     required: true,
-//   },
+  UserName: {
+    type: String,
+    required: true,
+  },
+  Address: {
+    type: String,
+    required: true,
+  },
+  OrderedItems: {
+    type: String,
+    required: true,
+  },
+  OrderedDate: {
+    type: String,
+    required: true,
+  },
+  DeliveryDate: {
+    type: String,
+    required: true,
+  },
+  DeliveryPartner: {
+    type: String,
+    required: true,
+  },
+  TotlePrice: {
+    type: String,
+    required: true,
+  },
+  
 });
 module.exports =
   mongoose.models.TodoDataModel || mongoose.model("TodoDataModel", NoteSchema);
